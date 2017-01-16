e1939b512f39dbc2982bc3acff5a12f8dd699efe
Justin Thomas <jthomas0209@gmail.com>
2017-01-16 14:08:15 -0500

    changed indentation setings for Github

--- node-weather-app/app-promise.js
+++ node-weather-app/app-promise.js
@@ -17,88 +17,88 @@ const FORECAST_KEY = process.env.FORECAST_IO_SECRET;
 //in this case addresss must be givem as a string
 //also address has the alias 'a'
 const argv = yargs
-	.options({
-		a:{
-			//demand: true,
-			alias: 'address',
-			describe: 'Address to fetch weather for',
-			string: true
-			//default: process.env.HOME_ADDRESS
-		}
-	}).help()
-	.alias('help', 'h')
-	.argv;
+  .options({
+    a:{
+      //demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+      //default: process.env.HOME_ADDRESS
+    }
+  }).help()
+  .alias('help', 'h')
+  .argv;
 
 
 if(!argv.address){
-	processGeocode()
-	.then((resp) => forecastAnalysis(resp))
-	.catch((error) => console.log(error));
+  processGeocode()
+  .then((resp) => forecastAnalysis(resp))
+  .catch((error) => console.log(error));
 } else {
-	encodedURL = generateAddressURL(process.env.HOME_ADDRESS);
-	forecastAnalysis(encodedURL);
+  encodedURL = generateAddressURL(process.env.HOME_ADDRESS);
+  forecastAnalysis(encodedURL);
 }
 
 function forecastAnalysis (geocodeURL) {
-	axios.get(geocodeURL).then((geocodeRes) => {
-		if (geocodeRes.data.status === 'ZERO_RESULTS'){
-			throw new Error("Address not found");
-		} else {
-			let formattedAddress = geocodeRes.data.results[0].formatted_address;
-			let latitude = geocodeRes.data.results[0].geometry.location.lat
-			let longitude = geocodeRes.data.results[0].geometry.location.lng
-			let weatherURL = `https://api.darksky.net/forecast/${FORECAST_KEY}/${latitude},${longitude}`
-			console.log(`Retreiving tempertature for ${formattedAddress}...`);
-			return axios.get(weatherURL);
-		}
-	}).then((weatherRes) => {
-		if(weatherRes.status === 200){
-			console.log("Here's the weekly forecast");
-			console.log('');
-			weatherRes.data.daily.data.forEach((day) => {
-				outputTemperature(day);
-			});
-			console.log('===================================================');
-		} else {
-			throw new Error("Unable to connect to forecast.io");
-		}
-	}).catch((error) => {
-		if(error.code === 'ENOTFOUND'){
-			console.log('Unable to connect to API');
-		} else {
-			console.log(error.message);
-		}
-	});
+  axios.get(geocodeURL).then((geocodeRes) => {
+    if (geocodeRes.data.status === 'ZERO_RESULTS'){
+      throw new Error("Address not found");
+    } else {
+      let formattedAddress = geocodeRes.data.results[0].formatted_address;
+      let latitude = geocodeRes.data.results[0].geometry.location.lat
+      let longitude = geocodeRes.data.results[0].geometry.location.lng
+      let weatherURL = `https://api.darksky.net/forecast/${FORECAST_KEY}/${latitude},${longitude}`
+      console.log(`Retreiving tempertature for ${formattedAddress}...`);
+      return axios.get(weatherURL);
+    }
+  }).then((weatherRes) => {
+    if(weatherRes.status === 200){
+      console.log("Here's the weekly forecast");
+      console.log('');
+      weatherRes.data.daily.data.forEach((day) => {
+        outputTemperature(day);
+      });
+      console.log('===================================================');
+    } else {
+      throw new Error("Unable to connect to forecast.io");
+    }
+  }).catch((error) => {
+    if(error.code === 'ENOTFOUND'){
+      console.log('Unable to connect to API');
+    } else {
+      console.log(error.message);
+    }
+  });
 }
 
 function outputTemperature(day) {
-	console.log('===================================================');
-	console.log(new Date(day.time * 1000).toDateString());
-	console.log(`Summary: ${day.summary}`);
-	console.log(`High: ${day.temperatureMax} | Feels like ${day.apparentTemperatureMax}`);
-	console.log(`Low: ${day.temperatureMin} | Feels like ${day.apparentTemperatureMin}`);
+  console.log('===================================================');
+  console.log(new Date(day.time * 1000).toDateString());
+  console.log(`Summary: ${day.summary}`);
+  console.log(`High: ${day.temperatureMax} | Feels like ${day.apparentTemperatureMax}`);
+  console.log(`Low: ${day.temperatureMin} | Feels like ${day.apparentTemperatureMin}`);
 }
 
 function processGeocode() {
-	return new Promise((resolve, reject) => {
-		exec(cmd, (error, stdout, stderror) => {
-			if(!error){
-				let coordinates = stdout.split('\n').map((substring, index) => {
-					if(index <= 1){
-						return substring.split(": ")[1];
-					}
-				});
-				resolve (`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates[0]},${coordinates[1]}`);
-			} else if (process.env.HOME_ADDRESS !== undefined) { 
-				resolve (generateAddressURL(process.env.HOME_ADDRESS));
-			} else {
-				reject('You have no default address saved in .env file.');
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderror) => {
+      if(!error){
+        let coordinates = stdout.split('\n').map((substring, index) => {
+          if(index <= 1){
+            return substring.split(": ")[1];
+          }
+        });
+        resolve (`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coordinates[0]},${coordinates[1]}`);
+      } else if (process.env.HOME_ADDRESS !== undefined) { 
+        resolve (generateAddressURL(process.env.HOME_ADDRESS));
+      } else {
+        reject('You have no default address saved in .env file.');
+      }
+    });
+  });
 }
 
 function generateAddressURL(address) {
-	let encodedAddress = encodeURIComponent(argv.address);
-	return `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+  let encodedAddress = encodeURIComponent(argv.address);
+  return `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 }
